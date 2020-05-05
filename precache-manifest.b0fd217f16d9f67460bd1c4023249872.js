eca8445401e356ea8f0553ffb52d48ac26b62483
Chinedu Alex <nwekealexchinedu@gmail.com>
2020-05-05 03:58:45 +0100

    Updates

---
+++ precache-manifest.b0fd217f16d9f67460bd1c4023249872.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c92de28ced11cbf914667a911a7c560",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "ac4dd211f8e68330395c",
+    "url": "/react-todo-list/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "18cb633b26b15513f9a5",
+    "url": "/react-todo-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ac4dd211f8e68330395c",
+    "url": "/react-todo-list/static/js/2.c4f4a133.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.c4f4a133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18cb633b26b15513f9a5",
+    "url": "/react-todo-list/static/js/main.b8dceb22.chunk.js"
+  },
+  {
+    "revision": "e75525395dbd0ff8bb61",
+    "url": "/react-todo-list/static/js/runtime-main.7ccca816.js"
+  }
+]);
