ea1907bb2f7dafecaa11baf6ef50d1528e16afa3
Chinedu Alex <nwekealexchinedu@gmail.com>
2020-05-17 23:27:15 +0100

    remove some unneccersary comments

--- src/App.js
+++ src/App.js
@@ -13,32 +13,9 @@ class App extends React.Component {
           <div className="row">
             <div className="col-10 mx-auto col-md-8 mt-4">
               <h3 className="text-capitalize text-center">Todo List App</h3>
-              <TodoInput
-              // item={this.state.item}
-              // handleChange={this.handleChange}
-              // handleSubmit={this.handleSubmit}
-              // editItem={this.state.editItem}
-              // invalidInput={this.state.invalidInput}
-              // moreDetails={this.state.moreDetails}
-              // showDetail={this.showDetail}
-              // showTime={this.state.showTime}
-              // handleTime={this.handleTime}
-              // time={this.state.time}
-              // date={this.state.date}
-              />
-              <TodoList
-              // items={this.state.items}
-              // clearList={this.clearList}
-              // handleDelete={this.handleDelete}
-              // handleEdit={this.handleEdit}
-              // taskCompleted={this.taskCompleted}
-              />
-              <CompletedTask
-              // items={this.state.completedTask}
-              // taskUncompleted={this.taskUncompleted}
-              // clearCompletedTask={this.clearCompletedTask}
-              // deleteCompletedTask={this.deleteCompletedTask}
-              />
+              <TodoInput />
+              <TodoList />
+              <CompletedTask />
             </div>
           </div>
         </div>
